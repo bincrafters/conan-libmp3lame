1fef71b0a4852975a8c1c3bf71e8671582708719
SSE4 <tomskside@gmail.com>
2018-03-04 16:03:53 +0700

    - spaces

--- build.py
+++ build.py
@@ -6,16 +6,19 @@ from bincrafters import build_template_default
 from bincrafters import build_shared
 import os
 
+
 def add_build_requires(builds):
     if os.getenv('MINGW_CONFIGURATIONS', ''):
         return map(add_required_installers, builds)
     else:
         return builds
 
+
 def add_required_installers(build):
-    installers = ['mingw_installer/1.0@conan/stable','msys2_installer/latest@bincrafters/stable']
-    build.build_requires.update({"*" : installers})
+    installers = ['mingw_installer/1.0@conan/stable', 'msys2_installer/latest@bincrafters/stable']
+    build.build_requires.update({"*": installers})
     return build
+
 
 if __name__ == "__main__":
 
